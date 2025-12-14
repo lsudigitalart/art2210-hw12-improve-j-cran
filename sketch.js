bbf91ef06329fb405429d249887c6446d6e137e5
Sebastian Crandall <jocelynnecrandall@gmail.com>
2025-12-13 19:05:35 -0600

    hey section added

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-let jamPlay, fft, amp, level, cheeseSize, trumpetSize, cheeseImg, trumpetImg, addImage1, addImage2, addImage3, addImage4, cheeseLocation;
-let randomX,randomY
+let jamPlay, fft, amp, level, cheeseSize, trumpetSize, cheeseImg, trumpetImg, addImage1, addImage2, addImage3, addImage4,cheeseLocation;
+let endHeys, addHey1, addHey2, addHey3, addHey4, addHey5, addHey6,addHey7,addHey8,addHey9,addHey10;
 let hue = 0;
 
 function preload(){
@@ -12,7 +12,8 @@ function setup(){
     createCanvas(1200, 600);
     imageMode(CENTER);
 
-    fft=new p5.FFT();
+
+    fft= new p5.FFT();
     amp = new p5.Amplitude();
     cheeseSize=0;
 
@@ -20,16 +21,25 @@ function setup(){
     jamPlay.addCue(1.1,makeImage2)
     jamPlay.addCue(2.2,makeImage3)
     jamPlay.addCue(3.1,makeImage4)
-    jamPlay.addCue(5.1,makeImage5)
 
+    jamPlay.addCue(20.6,makeHey1) // Hey section, couldnt get for loop to randomize in the way i wanted it to
+    jamPlay.addCue(20.2,makeHey2)
+    jamPlay.addCue(20.3,makeHey3)
+    jamPlay.addCue(20.8,makeHey4)
+    jamPlay.addCue(21.2,makeHey5)
+    jamPlay.addCue(21.3,makeHey6)
+    jamPlay.addCue(20.8,makeHey7)
+    jamPlay.addCue(20.5,makeHey8)
+    jamPlay.addCue(20.4,makeHey9)
+    jamPlay.addCue(20.1,makeHey10)
+
+    jamPlay.addCue(22,makeEndHeys)
 }
 
 function draw(){
     background(hue,280,180);
     hue+=0.5 // slow colour transition 
 
-    randomX = random(0,1200);
-    randomY = random(0,600);
 
     fill(0);
     textFont("courier new")                   
@@ -70,8 +80,58 @@ function draw(){
         pop(); 
     }
 
-    if(addImage5===true){
-        image(cheeseImg,900,100,500,500);
+    if(addHey1===true){   // hey section
+        image(cheeseImg,100,200,100,100);
+    }
+    
+    if(addHey2===true){
+        image(cheeseImg,670,390,100,100);
+    }
+    
+    if(addHey3===true){
+        image(cheeseImg,320,180,80,80);
+    }
+    
+    if(addHey4===true){
+        image(cheeseImg,780,370,100,100);
+    }
+    
+    if(addHey5===true){
+        image(cheeseImg,1110,130,120,120);
+    }
+    
+    if(addHey6===true){
+        image(cheeseImg,490,550,100,100);
+    }
+
+    if(addHey7===true){
+        image(cheeseImg,1010,410,110,110);
+    }
+    
+    if(addHey8===true){
+        image(cheeseImg,800,120,100,100);
+    }
+    
+    if(addHey9===true){
+        image(cheeseImg,450,270,100,100);
+    }
+
+    if(addHey10===true){
+        image(cheeseImg,180,500,100,100);
+    }
+    
+
+    if(endHeys===true){  // remove hey cheeses
+        addHey1 = false;
+        addHey2 = false;
+        addHey3 = false;
+        addHey4 = false;
+        addHey5 = false;
+        addHey6 = false;
+        addHey7 = false;
+        addHey8 = false;
+        addHey9 = false;
+        addHey10 = false;
     }
 
 }
@@ -103,6 +163,46 @@ function makeImage4(){
     addImage4=true
 }
 
-function makeImage5(){
-    addImage5=true
+function makeHey1(){
+    addHey1=true
+}
+
+function makeHey2(){
+    addHey2=true
+}
+
+function makeHey3(){
+    addHey3=true
+}
+
+function makeHey4(){
+    addHey4=true
+}
+
+function makeHey5(){
+    addHey5=true
+}
+
+function makeHey6(){
+    addHey6=true
+}
+
+function makeHey7(){
+    addHey7=true
+}
+
+function makeHey8(){
+    addHey8=true
+}
+
+function makeHey9(){
+    addHey9=true
+}
+
+function makeHey10(){
+    addHey10=true
+}
+
+function makeEndHeys(){
+    endHeys=true
 }
